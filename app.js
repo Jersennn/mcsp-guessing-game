a5f6aa5e3dca33388ef13efe8385232c9027649d
Jersen Reyes <jersen222@gmail.com>
2022-11-08 12:37:55 -0800

    finished project

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-var count = 0;
-let prevGuess = [];
-let history = {}
-const userName = prompt("What is your name?");
-history[username]
-
-
-function play(){
+const history = {};
+function success(numGuesses, userName) {
+    const prevAttempt = history[userName];
+            const difference = prevAttempt - numGuesses;
+            if ( difference < 0) {
+                alert(`That's correct ${userName}! You did better in your last game by ${Math.abs(difference)} fewer guesses!`);
+            } else if (difference > 0) {
+                alert(`Thats correct ${userName}! And you beat your previous attempt by ${difference} fewer guesses!`);
+            } else {
+                alert(`Thats correct ${userName}! You tied your previous attempt!`);
+            }
+}
+function play() {
+    let numGuesses = 1;
+    const userName = prompt("What is your name?");
     const secretNum = Math.floor(Math.random() * 10) + 1;
+
   while (true) {
     const guess =  prompt("Guess a number.");
-    count++
     if (guess === null) {
         alert("Goodbye!");
         break;
@@ -20,15 +27,23 @@ function play(){
     if (Number.isNaN(numGuess)) {
         alert("Please enter a valid number");
     } else if (numGuess < secretNum) {
+        numGuesses++
          alert(`Sorry ${userName} guess higher`)
-         prevGuess.push(numGuess)
      } else if (numGuess > secretNum) {
+        numGuesses++
          alert(`Sorry ${userName} guess lower`)
-         prrevGuess.push(numGuess)
      } else {
-         alert(`Correct! You're previous guesses were:  ${prevGuess.join(" , ")}`);
-         const playAgain = prompt("Do you want to play again?");
-         if (playAgain.toLowerCase() === "yes") {
+
+        if (!history.hasOwnProperty(userName)) {
+            alert(`Correct! It took you ${numGuesses} guesses!`)
+
+        } else {
+            success(numGuesses, userName)
+        }
+
+        history[userName] = numGuesses
+        const playAgain = prompt("Do you want to play again?");
+         if (playAgain.toLowerCase() == "yes") {
             play()
          } else {
          break;
